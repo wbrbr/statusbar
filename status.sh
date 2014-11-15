2393ff00a30ab817b273cc50f7c68fdc16cd5adc
Nounours Heureux <schtroumps31@gmail.com>
2014-11-15 07:53:12 +0100

    Popups now spawn at cursor position

--- status.sh
+++ status.sh
@@ -3,10 +3,10 @@
 Vol() {
     mute=$(amixer get Master | grep Mono: | cut -d " " -f 8)
     if [[ $mute == "[off]" ]]; then
-        echo -n "^ca(1,volume_popup)^fg(orange)^i("$HOME/dzen-icons/spkr_02.xbm") : Muet  ^fg()^ca()| "
+        echo -n "^ca(1,./volume_popup)^fg(orange)^i("$HOME/dzen-icons/spkr_02.xbm") : Muet  ^fg()^ca()| "
     else 
         volume=$(amixer get Master | grep Mono: | cut -d " " -f 6)
-        echo -n "^ca(1,volume_popup)^fg(orange)^i("$HOME/dzen-icons/spkr_01.xbm") : $volume ^fg()^ca()| "
+        echo -n "^ca(1,./volume_popup)^fg(orange)^i("$HOME/dzen-icons/spkr_01.xbm") : $volume ^fg()^ca()| "
     fi
     return
 }
@@ -27,28 +27,28 @@ Battery() {
 
 Disk() {
     space=$(df -h /home|sed '1d'|awk '{print $4}')
-    echo -n "^ca(1, disk_popup)^fg(green)^i($HOME/dzen-icons/diskette.xbm) : $space ^fg()^ca()| "
+    echo -n "^ca(1, ./disk_popup)^fg(green)^i($HOME/dzen-icons/diskette.xbm) : $space ^fg()^ca()| "
     return
 }
 
 Time() {
     hour=$(date +"%H:%M:%S")
     date=$(date +"%a %d %b")
-    echo -n "^ca(1,fortune_popup)^fg(red)$date ^fg()^ca()| ^ca(1,~/bin/clock_popup)^i($HOME/dzen-icons/clock.xbm) : $hour^ca()"
+    echo -n "^ca(1,./fortune_popup)^fg(red)$date ^fg()^ca()| ^ca(1,./clock_popup)^i($HOME/dzen-icons/clock.xbm) : $hour^ca()"
     return
 }
 
 Music() {
     play=$(mpc current)
     if [[ $play ]]; then
-        echo -n "^fg(blue)^ca(1, music_popup)^i($HOME/dzen-icons/note.xbm) : $play^ca()^fg() | "
+        echo -n "^fg(blue)^ca(1, ./music_popup)^i($HOME/dzen-icons/note.xbm) : $play^ca()^fg() | "
     fi
     return
 }
 
 Update() {
     updates=$(wc -l < ~/updates)
-    echo -n "^ca(1,update_popup)^fg(#4BB5C1)^i($HOME/dzen-icons/arch.xbm) : $updates^fg()^ca() | "
+    echo -n "^ca(1,./update_popup)^fg(#4BB5C1)^i($HOME/dzen-icons/arch.xbm) : $updates^fg()^ca() | "
 }
 
 Mail() {
@@ -61,22 +61,22 @@ Mail() {
     fi
 }
 
-    Print () {
-        Vol
-        Disk
-        Wifi
-        #Battery
-        Music
-        Update
-        Time
-        Mail
-        echo
-        return
-    }
+Print () {
+    Vol
+    Disk
+    Wifi
+    #Battery
+    Music
+    Update
+    Time
+    Mail
+    echo
+    return
+}
 
-    while true 
-    do
-        sleep 1
-        echo "$(Print)" 
-    done | dzen2 -p -fn  -*-ohsnapu-medium-r-normal-*-11-*-*-*-*-*-*-* -dock 
+while true 
+do
+    sleep 1
+    echo "$(Print)" 
+done | dzen2 -p -fn  -*-ohsnapu-medium-r-normal-*-11-*-*-*-*-*-*-* -dock 
 
