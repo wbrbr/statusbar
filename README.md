683fa39d7ef881da8884deb908b982b1acef44f1
Nounours Heureux <schtroumps31@gmail.com>
2014-12-12 18:47:46 +0100

    Added README and icons

---
+++ README.md
@@ -0,0 +1,16 @@
+This is a little status bar I made for my own purpose. It uses dzen2, a very nice and flexible tool for making status bar. 
+Here is a video of it in action: https://www.youtube.com/watch?v=Q5FIPhRlieA 
+I did this for myself, so I really didn't care about code flexibility. But know I feel ashamed of that, so I will try to improve my code. 
+I did not create the icons, these are sm4tik's ones.
+
+# Dependencies
+
+Here is the (huge) list of dependencies:   
+- dzen2
+- gdbar (should be included with dzen2)
+- df
+- acpi
+- date
+- mpd & mpc
+- for the update checker I redirect the output of `updatechecker` in a file
+- for the mail checker I created a script that creates a file with the number of new mails
--- status.sh
+++ status.sh
@@ -3,11 +3,11 @@
 Vol() {
     mute=$(amixer get Master | grep Mono: | cut -d " " -f 8)
     if [[ $mute == "[off]" ]]; then
-        echo -n "^ca(1,./volume_popup)^fg(orange)^i("$HOME/dzen-icons/spkr_02.xbm") : Muet ^fg()^ca()| "
+        echo -n "^ca(1,./volume_popup)^fg(orange)^i("icons/spkr_02.xbm") : Muet ^fg()^ca()| "
     else 
         volume=$(amixer get Master | grep Mono: | cut -d " " -f 6 | sed -s 's/\[\(.*\)%\]/\1/')
         volume_bar=$(echo $volume | gdbar -h 2 -w 50 -fg orange)
-        echo -n "^ca(1,./volume_popup)^fg(orange)^i("$HOME/dzen-icons/spkr_01.xbm") : $volume_bar ^fg()^ca()| "
+        echo -n "^ca(1,./volume_popup)^fg(orange)^i("icons/spkr_01.xbm") : $volume_bar ^fg()^ca()| "
     fi
     return
 }
@@ -16,41 +16,41 @@ Wifi() {
     ssid=$(iwconfig wlp11s0 |grep ESSID| awk '{print $NF}'|cut -d ":" -f 2)
     ip=$(ifconfig wlp11s0 |grep 192|grep inet)
     ip=$(echo $ip|cut -d " " -f 2)
-    echo -n "^fg(purple)^i($HOME/dzen-icons/wifi_02.xbm) : $ssid  $ip ^fg()| "
+    echo -n "^fg(purple)^i(icons/wifi_02.xbm) : $ssid  $ip ^fg()| "
     return
 }
 
 Battery() {
     charge=$(acpi|cut -d "," -f 2)
-    echo -n "^fg(yellow)^i($HOME/dzen-icons/bat_full_02.xbm) : $charge^fg() | "
+    echo -n "^fg(yellow)^i(icons/bat_full_02.xbm) : $charge^fg() | "
     return
 }
 
 Disk() {
     space=$(df -h /home|sed '1d'|awk '{print $5}' | sed 's/\(.*\)%/\1/')
     space_bar=$(echo $space | gdbar -h 2 -w 50 -fg green)
-    echo -n "^ca(1, ./disk_popup)^fg(green)^i($HOME/dzen-icons/diskette.xbm) : $space_bar ^fg()^ca()| "
+    echo -n "^ca(1, ./disk_popup)^fg(green)^i(icons/diskette.xbm) : $space_bar ^fg()^ca()| "
     return
 }
 
 Time() {
     hour=$(date +"%H:%M:%S")
     date=$(date +"%a %d %b")
-    echo -n "^ca(1,./fortune_popup)^fg(red)$date ^fg()^ca()| ^ca(1,./clock_popup)^i($HOME/dzen-icons/clock.xbm) : $hour^ca()"
+    echo -n "^ca(1,./fortune_popup)^fg(red)$date ^fg()^ca()| ^ca(1,./clock_popup)^i(icons/clock.xbm) : $hour^ca()"
     return
 }
 
 Music() {
     play=$(mpc current)
     if [[ $play ]]; then
-        echo -n "^fg(blue)^ca(1, ./music_popup)^i($HOME/dzen-icons/note.xbm) : $play^ca()^fg() | "
+        echo -n "^fg(blue)^ca(1, ./music_popup)^i(icons/note.xbm) : $play^ca()^fg() | "
     fi
     return
 }
 
 Update() {
     updates=$(wc -l < ~/updates)
-    echo -n "^ca(1,./update_popup)^fg(#4BB5C1)^i($HOME/dzen-icons/arch.xbm) : $updates^fg()^ca() | "
+    echo -n "^ca(1,./update_popup)^fg(#4BB5C1)^i(icons/arch.xbm) : $updates^fg()^ca() | "
 }
 
 Mail() {
@@ -59,7 +59,7 @@ Mail() {
         touch ~/newmail
     fi
     if [[ $mailnbr != "0" ]]; then
-        echo -n " | ^fg(#FF358B)^i($HOME/dzen-icons/mail.xbm) : $mailnbr^fg()"
+        echo -n " | ^fg(#FF358B)^i(icons/mail.xbm) : $mailnbr^fg()"
     fi
 }
 
